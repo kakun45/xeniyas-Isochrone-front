b820ed9648885d9a04a7095cdb9f69bc9deca22a
kakun45 <email4blackkat@gmail.com>
2023-04-15 00:50:56 -0400

    route / vs /map

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/map" element={<Isochrone />} />;
+        <Route path="/" element={<Isochrone />} />;
       </Routes>
     </BrowserRouter>
   );
